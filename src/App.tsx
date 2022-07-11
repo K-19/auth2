5c3db0161edbb92603e59feecf88e76885f6e857
k-19 <sergeifilinovich01@gmail.com>
2022-07-11 03:20:36 +0300

    modal windows for registration and password reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import style from './App.module.css';
+import Footer from "./Footer/Footer";
+import Content from "./Content/Content";
+import React, {useState} from "react";
+import Modal from "./Modal/Modal";
+import RegPanel from "./RegPanel/RegPanel";
+import PassResetPanel from "./PassResetPanel/PassResetPanel";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [regModalActive, setRegModalActive] = useState(false);
+    const [passResetModalActive, setPassResetModalActive] = useState(false);
+    return (
+        <div className={style.app}>
+            <Content setRegModalActive={setRegModalActive} setPassResetModalActive={setPassResetModalActive}/>
+            <Footer/>
+            <Modal active={regModalActive} setActive={setRegModalActive}>
+                <RegPanel setRegModalActive={setRegModalActive}/>
+            </Modal>
+            <Modal active={passResetModalActive} setActive={setPassResetModalActive}>
+                <PassResetPanel setPassResetModalActive={setPassResetModalActive}/>
+            </Modal>
+        </div>
+    )
 }
 
 export default App;
